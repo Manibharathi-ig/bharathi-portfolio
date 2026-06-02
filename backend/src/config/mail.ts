2d67a860c7c49e8f457fa9c5414b8a71a8e09f09
Mani Bharathi <manibharathixplore@gmail.com>
2026-06-02 19:57:40 +0530

    Remove email functionality

--- backend/src/config/mail.ts
+++ backend/src/config/mail.ts
@@ -1,21 +1,21 @@
-import nodemailer from "nodemailer";
+// import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // true only for port 465
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false, // true only for port 465
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
 
-transporter.verify((error, success) => {
-  if (error) {
-    console.log("MAIL ERROR:", error);
-  } else {
-    console.log("MAIL SERVER READY");
-  }
-});
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.log("MAIL ERROR:", error);
+//   } else {
+//     console.log("MAIL SERVER READY");
+//   }
+// });
 
-export default transporter;
+// export default transporter;
--- backend/src/controllers/contactController.ts
+++ backend/src/controllers/contactController.ts
@@ -35,26 +35,26 @@ if (!validation.success) {
       },
     });
 
-    console.log("Before mail");
+//     console.log("Before mail");
 
-    await transporter.sendMail({
-  from: process.env.EMAIL_USER,
-  to: process.env.EMAIL_USER,
+//     await transporter.sendMail({
+//   from: process.env.EMAIL_USER,
+//   to: process.env.EMAIL_USER,
 
-  subject: `New Portfolio Contact - ${name}`,
+//   subject: `New Portfolio Contact - ${name}`,
 
-  html: `
-    <h2>New Contact Received</h2>
+//   html: `
+//     <h2>New Contact Received</h2>
 
-    <p><strong>Name:</strong> ${name}</p>
-    <p><strong>Email:</strong> ${email}</p>
-    <p><strong>Opportunity:</strong> ${opportunityType}</p>
-    <p><strong>Project:</strong> ${projectType}</p>
-    <p><strong>Message:</strong> ${message}</p>
-  `,
-});
+//     <p><strong>Name:</strong> ${name}</p>
+//     <p><strong>Email:</strong> ${email}</p>
+//     <p><strong>Opportunity:</strong> ${opportunityType}</p>
+//     <p><strong>Project:</strong> ${projectType}</p>
+//     <p><strong>Message:</strong> ${message}</p>
+//   `,
+// });
 
-console.log("After mail");
+// console.log("After mail");
 
     res.status(201).json({
       success: true,
